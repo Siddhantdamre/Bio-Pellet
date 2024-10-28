829f46a63ff4dc463d7ddc6bdfca8999757e6596
Kushagra Sachdeva <72493826+Kushagra-sachdeva@users.noreply.github.com>
2024-10-28 21:03:02 +0530

    Add files via upload

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import Img1 from "../../assets/products/product1.png";
+import Img2 from "../../assets/products/product2.png";
+import Img3 from "../../assets/products/product3.png";
+import Img4 from "../../assets/products/product4.png";
+import { FaStar } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+
+const ProductsData = [
+ 
+];
+
+const Products = () => {
+  return (
+    <div className="mt-14 mb-12">
+      <div className="container">
+        <div className="text-center mb-10 max-w-[600px] mx-auto">
+        </div>
+        <div>
+          <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 place-items-center gap-5">
+            {ProductsData.map((data) => (
+              <div
+                data-aos="fade-up"
+                data-aos-delay={data.aosDelay}
+                key={data.id}
+                className="space-y-3"
+              >
+                <img
+                  src={data.img}
+                  alt=""
+                  className="h-[220px] w-[150px] object-cover rounded-md"
+                />
+                <div>
+                  <h3 className="font-semibold">{data.title}</h3>
+                  <p className="text-sm text-gray-600">{data.color}</p>
+                  <div className="flex items-center gap-1">
+                    <FaStar className="text-yellow-400" />
+                    <span>{data.rating}</span>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="flex justify-center">
+           
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
